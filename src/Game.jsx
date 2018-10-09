8cf7324ce3ba9a4344c7937c85566bbd7cd40b0f
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-10-09 07:53:10 +0100

    :construction:

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Weapons from './Weapons';
 import Score from './Score';
-// import logic from './logic';
+import { logic } from './logic';
 
 class Game extends Component {
   constructor(props) {
@@ -9,22 +9,29 @@ class Game extends Component {
     this.handleChoice = this.handleChoice.bind(this);
     this.state = {
       player1: {
-        name: 'Player 1',
-        wins: 'x',
-        losses: 'y',
-        draws: 'z'
+        name: 1,
+        // wins: 0,
+        // losses: 0,
+        // draws: 0
       },
       player2: {
-        name: 'Player 2',
-        wins: 'a',
-        losses: 'b',
-        draws: 'c'
+        name: 2,
+        // wins: 0,
+        // losses: 0,
+        // draws: 0
       }
     };
   }
 
   handleChoice(type, player) {
-    console.log('CLICK SUCCESSFUL');
+    // console.log('LOGIC: ', logic);
+    // console.log('PLAYER ', player, 'CHOSE WEAPON: ', type);
+
+    if (player === 1) {
+      // find player 1 type object in logic object, then find player 2 type in that object
+    }
+    
+    
   }
 
   render() {
--- src/Weapons.jsx
+++ src/Weapons.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import Weapon from './Weapon'
+import Weapon from './Weapon';
+import { ROCK, PAPER, SCISSORS } from './logic';
+
 
 const Weapons = ({ player, click }) => {
   return (
     <div>
-      <h1>{player.name}</h1>
-      <h2>Current score - Wins: {player.wins}, Losses: {player.losses}, Draws: {player.draws}</h2>
-      <Weapon type='rock' click={click} player={player.name} />
-      <Weapon type='paper' click={click} player={player.name} />
-      <Weapon type='scissors' click={click} player={player.name} />
+      <h1>Player {player.name}</h1>
+      {/* <h2>Current score - Wins: {player.wins}, Losses: {player.losses}, Draws: {player.draws}</h2> */}
+      <Weapon type={ROCK} click={click} player={player.name} />
+      <Weapon type={PAPER} click={click} player={player.name} />
+      <Weapon type={SCISSORS} click={click} player={player.name} />
     </div>
   )
 }
